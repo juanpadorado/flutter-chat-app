c0fc16429628d5c0b373787fee1288c6a47202e7
juanpadorado <juanpadorado@gmail.com>
2021-01-01 18:06:39 -0500

    Cambio en direccion de server socket desplegado

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -2,7 +2,10 @@ import 'dart:io';
 
 class Environment {
 
-  static String apiUrl = Platform.isAndroid ? 'http://10.0.2.2:3000/api' : 'http://192.168.1.3:3000/api';
-  static String socketUrl = Platform.isAndroid ? 'http://10.0.2.2:3000' : 'http://192.168.1.3:3000';
+  /*static String apiUrl = Platform.isAndroid ? 'http://10.0.2.2:3000/api' : 'http://192.168.1.3:3000/api';
+  static String socketUrl = Platform.isAndroid ? 'http://10.0.2.2:3000' : 'http://192.168.1.3:3000';*/
+
+  static String apiUrl = 'https://chat-server-socket-node.herokuapp.com/api';
+  static String socketUrl = 'https://chat-server-socket-node.herokuapp.com/';
 
 }
